b8c6bdf317a65f954b43e00ba714d8d26577ecac
Kunal Taneja <kunaltaneja907@gmail.com>
2026-02-11 12:39:45 +0530

    Fix import path for NavBar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import NavBar from "./Components/Navbar";
+import NavBar from "./Components/NavBar";
 import { Outlet } from "react-router-dom";
 import Footer from "./Components/Footer";
 
